534bb2378d29bc7dfcc47aaf9c94ec43ba75ab3e
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-02-21 13:06:55 -0800

    some extremely low hanging fruit tests

--- errors/parsing_test.go
+++ errors/parsing_test.go
@@ -11,4 +11,8 @@ func TestParseError(t *testing.T) {
 	err := NewParseError("Content-Type", "header", "application(", errors.New("unable to parse"))
 	assert.Equal(t, 400, err.Code())
 	assert.Equal(t, "parsing Content-Type header from \"application(\" failed, because unable to parse", err.Error())
+
+	err = NewParseError("Content-Type", "", "application(", errors.New("unable to parse"))
+	assert.Equal(t, 400, err.Code())
+	assert.Equal(t, "parsing Content-Type from \"application(\" failed, because unable to parse", err.Error())
 }
