1f5965877aec3ba3a17b78a560cfee1684382027
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-06-11 23:37:04 -0700

    adds a JoinByFormat function

--- swag/util_test.go
+++ swag/util_test.go
@@ -52,3 +52,66 @@ func TestContainsStringsCI(t *testing.T) {
 	assert.True(t, ContainsStringsCI(list, "AND"))
 	assert.False(t, ContainsStringsCI(list, "nuts"))
 }
+
+func TestSplitByFormat(t *testing.T) {
+	expected := []string{"one", "two", "three"}
+	for _, fmt := range []string{"csv", "pipes", "tsv", "ssv", "multi"} {
+
+		var actual []string
+		switch fmt {
+		case "multi":
+			assert.Nil(t, SplitByFormat("", fmt))
+			assert.Nil(t, SplitByFormat("blah", fmt))
+		case "ssv":
+			actual = SplitByFormat(strings.Join(expected, " "), fmt)
+			assert.EqualValues(t, expected, actual)
+		case "pipes":
+			actual = SplitByFormat(strings.Join(expected, "|"), fmt)
+			assert.EqualValues(t, expected, actual)
+		case "tsv":
+			actual = SplitByFormat(strings.Join(expected, "\t"), fmt)
+			assert.EqualValues(t, expected, actual)
+		default:
+			actual = SplitByFormat(strings.Join(expected, ","), fmt)
+			assert.EqualValues(t, expected, actual)
+		}
+	}
+}
+
+func TestJoinByFormat(t *testing.T) {
+	for _, fmt := range []string{"csv", "pipes", "tsv", "ssv", "multi"} {
+
+		lval := []string{"one", "two", "three"}
+		var expected []string
+		switch fmt {
+		case "multi":
+			expected = lval
+		case "ssv":
+			expected = []string{strings.Join(lval, " ")}
+		case "pipes":
+			expected = []string{strings.Join(lval, "|")}
+		case "tsv":
+			expected = []string{strings.Join(lval, "\t")}
+		default:
+			expected = []string{strings.Join(lval, ",")}
+		}
+		assert.Nil(t, JoinByFormat(nil, fmt))
+		assert.EqualValues(t, expected, JoinByFormat(lval, fmt))
+	}
+}
+
+func TestToFileName(t *testing.T) {
+	assert.Equal(t, "dog_walker", ToFileName("DogWalker"))
+}
+
+func TestToCommandName(t *testing.T) {
+	assert.Equal(t, "dog-walker", ToCommandName("DogWalker"))
+}
+
+func TestToHumanName(t *testing.T) {
+	assert.Equal(t, "dog walker", ToHumanNameLower("DogWalker"))
+}
+
+func TestToJSONName(t *testing.T) {
+	assert.Equal(t, "dogWalker", ToJSONName("DogWalker"))
+}
