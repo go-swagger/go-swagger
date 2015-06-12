964d5820d88d10777b882601aec5296278aa330f
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-06-12 12:10:38 -0700

    move test commands into circle.yml file

--- coverage.sh
+++ coverage.sh
@@ -5,4 +5,4 @@ set -e
 cd "${GOPATH/:*/}src/github.com/casualjim/go-swagger"
 
 godep go test -race ./...
-GOPATH=`godep path:$GOPATH` goveralls -service=circle-ci -repotoken=$COVERALLS_TOKEN
+GOPATH=`godep path`:$GOPATH goveralls -service=circle-ci -repotoken=$COVERALLS_TOKEN
