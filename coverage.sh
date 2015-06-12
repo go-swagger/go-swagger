c18369096fb6742078ce99ecc96653c2685fe16a
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-06-12 12:06:21 -0700

    also save coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -2,29 +2,7 @@
 
 set -e
 
-toolsgopath=${GOPATH/:*/}
-cd "$toolsgopath/src/github.com/casualjim/go-swagger"
+cd "${GOPATH/:*/}src/github.com/casualjim/go-swagger"
 
 godep go test -race ./...
-GOPATH=`godep path:$GOPATH` "$toolsgopath/bin/goveralls" -service=circle-ci
-# # Run test coverage on each subdirectories and merge the coverage profile.
-# echo "mode: count" > profile.cov
-#
-# # Standard go tooling behavior is to ignore dirs with leading underscores
-# for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d -not -path 'Godeps' -not -path './examples*' -not -path './fixtures*')
-# do
-# if ls $dir/*.go &> /dev/null; then
-#     godep go test -covermode=count -coverprofile=$dir/profile.tmp $dir
-#     if [ -f $dir/profile.tmp ]
-#     then
-#         cat $dir/profile.tmp | tail -n +2 >> profile.cov
-#         rm $dir/profile.tmp
-#     fi
-# fi
-# done
-#
-# godep go tool cover -func profile.cov
-
-# To submit the test coverage result to coveralls.io,
-# use goveralls (https://github.com/mattn/goveralls)
-# goveralls -coverprofile="profile.cov" -service="circle-ci" -repotoken="$COVERALLS_TOKEN"
+GOPATH=`godep path:$GOPATH` goveralls -service=circle-ci -repotoken=$COVERALLS_TOKEN
