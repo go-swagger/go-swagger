83e892cb945b50cc3654dbfb7d0d61e87d68f47b
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-06-12 12:44:03 -0700

    back to aggregated cover profile

--- coverage.sh
+++ coverage.sh
@@ -2,7 +2,22 @@
 
 set -e
 
-cd "${GOPATH/:*/}src/github.com/casualjim/go-swagger"
-
 godep go test -race ./...
-GOPATH=`godep path`:$GOPATH goveralls -service=circle-ci -repotoken=$COVERALLS_TOKEN
+
+# Standard go tooling behavior is to ignore dirs with leading underscores
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d -not -path 'Godeps' -not -path './examples*' -not -path './fixtures*' -not -path './swagger-ui')
+do
+if ls $dir/*.go &> /dev/null; then
+    godep go test -covermode=count -coverprofile=$dir/profile.tmp $dir
+    if [ -f $dir/profile.tmp ]
+    then
+        cat $dir/profile.tmp | tail -n +2 >> profile.cov
+        rm $dir/profile.tmp
+    fi
+fi
+done
+
+godep go tool cover -func profile.cov
+if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
+  goveralls -coverprofile=profile.cov -service=travis-ci -repotoken=$COVERALLS_TOKEN;
+fi
