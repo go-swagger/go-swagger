8302c5ae36780fac21f665383613b6d144a8e773
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-06-14 02:04:00 -0700

    moves spec related assets under spec package

--- assets/assets.go
+++
@@ -1,5 +0,0 @@
-// Package assets contains the embedded assets like the json schema json doc and the swagger 2.0 schema doc
-package assets
-
-//go:generate go-bindata -pkg=assets -prefix=../schemas -ignore=.*\.md ../schemas/...
-//go:generate perl -pi -e s,Json,JSON,g bindata.go
