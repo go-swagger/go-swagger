bdcdb70fd8e6c5c0cf9a961e5b1d67ec33317192
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-06-14 11:29:56 -0700

    continue to use ./coverage.sh to run tests

--- coverage.sh
+++ coverage.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-set -e
+set -e -x
+
+godep go test -v -race ./... | go-junit-report -dir $CIRCLE_TEST_REPORTS/go
 
 # Run test coverage on each subdirectories and merge the coverage profile.
-echo "mode: count" > profile.cov
-
+echo "mode: set" > profile.cov
+repo_pref="github.com/${CIRCLE_PROJECT_USERNAME-"$(basename `pwd`)"}/${CIRCLE_PROJECT_REPONAME-"$(basename `pwd`)"}/"
 # Standard go tooling behavior is to ignore dirs with leading underscores
 for dir in $(go list ./... | grep -v generator)
 do
-  godep go test -covermode=count -coverprofile=$dir/profile.tmp $dir
-  if [ -f $dir/profile.tmp ]
+  pth="${dir//*$repo_pref}"
+  godep go test -covermode=count -coverprofile=${pth}/profile.tmp $dir
+  if [ -f $pth/profile.tmp ]
   then
-      cat $dir/profile.tmp | tail -n +2 >> profile.cov
-      rm $dir/profile.tmp
+      cat $pth/profile.tmp | tail -n +2 >> profile.cov
+      rm $pth/profile.tmp
   fi
 done
 
 godep go tool cover -func profile.cov
+gocov convert profile.cov | gocov report
 gocov convert profile.cov | gocov-html > $CIRCLE_ARTIFACTS/coverage-$CIRCLE_BUILD_NUM.html
