f492d3935f42e70604f05bd79c5e6447785e9241
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-12-06 14:11:17 -0800

    add shippable.yml file

---
+++ scripts/shippable-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e -x
+
+mkdir -p ./shippable/codecoverage
+godep go test -v -race $(go list ./... | grep -v vendor) | go-junit-report -dir ./shippable/codecoverage/coverage.xml
+
+# Run test coverage on each subdirectories and merge the coverage profile.
+echo "mode: set" > profile.cov
+repo_pref="github.com/${USER-"$(basename `pwd`)"}/${REPO_NAME-"$(basename `pwd`)"}/"
+# Standard go tooling behavior is to ignore dirs with leading underscores
+set -x
+for dir in $(go list ./... | grep -v -E 'vendor|generator')
+do
+  pth="${dir//*$repo_pref}"
+  godep go test -covermode=set -coverprofile=${pth}/profile.tmp $dir
+  if [ -f $pth/profile.tmp ]
+  then
+      cat $pth/profile.tmp | tail -n +2 >> profile.cov
+      rm $pth/profile.tmp
+  fi
+done
+
+#set +x
+#godep go tool cover -func profile.cov
+#gocov convert profile.cov | gocov report
+#gocov convert profile.cov | gocov-html > $CIRCLE_ARTIFACTS/coverage-$CIRCLE_BUILD_NUM.html
