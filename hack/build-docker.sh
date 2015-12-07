93cf6ee23237ca8c178b265d67622a8edc8d502a
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-12-06 18:02:00 -0800

    use circle docker support

---
+++ hack/build-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+go test -v -race $(go list ./... | grep -v vendor) | go-junit-report -dir /usr/share/testresults
+
+# Run test coverage on each subdirectories and merge the coverage profile.
+echo "mode: count" > profile.cov
+repo_pref="github.com/${CIRCLE_PROJECT_USERNAME-"$(basename `pwd`)"}/${CIRCLE_PROJECT_REPONAME-"$(basename `pwd`)"}/"
+# Standard go tooling behavior is to ignore dirs with leading underscores
+for dir in $(go list ./... | grep -v -E 'vendor|generator')
+do
+  pth="${dir//*$repo_pref}"
+  go test -covermode=count -coverprofile=${pth}/profile.tmp $dir
+  if [ -f $pth/profile.tmp ]
+  then
+      cat $pth/profile.tmp | tail -n +2 >> profile.cov
+      rm $pth/profile.tmp
+  fi
+done
+
+go tool cover -func profile.cov
+gocov convert profile.cov | gocov report
+gocov convert profile.cov | gocov-html > /usr/share/coverage/coverage-${CIRCLE_BUILD_NUM-"0"}.html
+go build -o /usr/share/dist/swagger ./cmd/swagger
