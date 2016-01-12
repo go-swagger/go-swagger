dc046e73aa7c0588ebb995150df0dde70498cff2
Ivan Porto Carrero <ivan@flanders.co.nz>
2016-01-11 20:04:43 -0800

    fillout the code for the tutorial again

--- examples/tutorials/todo-list/server-complete/cmd/todo-list-server/configure_todo_list.go
+++ examples/tutorials/todo-list/server-complete/cmd/todo-list-server/configure_todo_list.go
@@ -2,16 +2,81 @@ package main
 
 import (
 	"net/http"
+	"sync"
+	"sync/atomic"
 
 	"github.com/go-swagger/go-swagger/errors"
 	"github.com/go-swagger/go-swagger/httpkit"
 	"github.com/go-swagger/go-swagger/httpkit/middleware"
+	"github.com/go-swagger/go-swagger/swag"
 
+	"github.com/go-swagger/go-swagger/examples/tutorials/todo-list/server-complete/models"
 	"github.com/go-swagger/go-swagger/examples/tutorials/todo-list/server-complete/restapi/operations"
 	"github.com/go-swagger/go-swagger/examples/tutorials/todo-list/server-complete/restapi/operations/todos"
 )
 
 // This file is safe to edit. Once it exists it will not be overwritten
+
+var items map[int64]*models.Item
+var lastID int64
+
+var itemsLock = &sync.Mutex{}
+
+func newItemID() int64 {
+	return atomic.AddInt64(&lastID, 1)
+}
+
+func addItem(item *models.Item) error {
+	if item == nil {
+		return errors.New(500, "item must be present")
+	}
+
+	itemsLock.Lock()
+	defer itemsLock.Unlock()
+
+	item.ID = newItemID()
+	items[item.ID] = item
+
+	return nil
+}
+
+func updateItem(id int64, item *models.Item) error {
+	if item == nil {
+		return errors.New(500, "item must be present")
+	}
+
+	itemsLock.Lock()
+	defer itemsLock.Unlock()
+
+	_, exists := items[id]
+	if !exists {
+		return errors.NotFound("not found: item %d", id)
+	}
+
+	item.ID = id
+	items[id] = item
+	return nil
+}
+
+func deleteItem(id int64) error {
+	itemsLock.Lock()
+	defer itemsLock.Unlock()
+
+	_, exists := items[id]
+	if !exists {
+		return errors.NotFound("not found: item %d", id)
+	}
+
+	delete(items, id)
+	return nil
+}
+
+func allItems(since int64, limit int32) (result []*models.Item) {
+	for _, v := range items {
+		result = append(result, v)
+	}
+	return
+}
 
 func configureAPI(api *operations.TodoListAPI) http.Handler {
 	// configure the api here
@@ -22,16 +87,34 @@ func configureAPI(api *operations.TodoListAPI) http.Handler {
 	api.JSONProducer = httpkit.JSONProducer()
 
 	api.TodosAddOneHandler = todos.AddOneHandlerFunc(func(params todos.AddOneParams) middleware.Responder {
-		return middleware.NotImplemented("operation todos.AddOne has not yet been implemented")
+		if err := addItem(params.Body); err != nil {
+			return todos.NewAddOneDefault(500).WithPayload(&models.Error{Code: 500, Message: err.Error()})
+		}
+		return todos.NewAddOneCreated().WithPayload(params.Body)
 	})
+
 	api.TodosDestroyOneHandler = todos.DestroyOneHandlerFunc(func(params todos.DestroyOneParams) middleware.Responder {
-		return middleware.NotImplemented("operation todos.DestroyOne has not yet been implemented")
+		if err := deleteItem(params.ID); err != nil {
+			return todos.NewDestroyOneDefault(500).WithPayload(&models.Error{Code: 500, Message: err.Error()})
+		}
+		return todos.NewDestroyOneNoContent()
 	})
+
 	api.TodosFindTodosHandler = todos.FindTodosHandlerFunc(func(params todos.FindTodosParams) middleware.Responder {
-		return middleware.NotImplemented("operation todos.FindTodos has not yet been implemented")
+		if params.Since == nil {
+			params.Since = swag.Int64(0)
+		}
+		if params.Limit == nil {
+			params.Limit = swag.Int32(20)
+		}
+		return todos.NewFindTodosOK().WithPayload(allItems(*params.Since, *params.Limit))
 	})
+
 	api.TodosUpdateOneHandler = todos.UpdateOneHandlerFunc(func(params todos.UpdateOneParams) middleware.Responder {
-		return middleware.NotImplemented("operation todos.UpdateOne has not yet been implemented")
+		if err := updateItem(params.ID, params.Body); err != nil {
+			return todos.NewUpdateOneDefault(500).WithPayload(&models.Error{Code: 500, Message: err.Error()})
+		}
+		return todos.NewUpdateOneOK().WithPayload(params.Body)
 	})
 
 	api.ServerShutdown = func() {}
--- swag/convert_types.go
+++ swag/convert_types.go
@@ -181,6 +181,65 @@ func IntValueMap(src map[string]*int) map[string]int {
 	return dst
 }
 
+// Int32 returns a pointer to of the int64 value passed in.
+func Int32(v int32) *int32 {
+	return &v
+}
+
+// Int32Value returns the value of the int64 pointer passed in or
+// 0 if the pointer is nil.
+func Int32Value(v *int32) int32 {
+	if v != nil {
+		return *v
+	}
+	return 0
+}
+
+// Int32Slice converts a slice of int64 values into a slice of
+// int32 pointers
+func Int32Slice(src []int32) []*int32 {
+	dst := make([]*int32, len(src))
+	for i := 0; i < len(src); i++ {
+		dst[i] = &(src[i])
+	}
+	return dst
+}
+
+// Int32ValueSlice converts a slice of int32 pointers into a slice of
+// int32 values
+func Int32ValueSlice(src []*int32) []int32 {
+	dst := make([]int32, len(src))
+	for i := 0; i < len(src); i++ {
+		if src[i] != nil {
+			dst[i] = *(src[i])
+		}
+	}
+	return dst
+}
+
+// Int32Map converts a string map of int32 values into a string
+// map of int32 pointers
+func Int32Map(src map[string]int32) map[string]*int32 {
+	dst := make(map[string]*int32)
+	for k, val := range src {
+		v := val
+		dst[k] = &v
+	}
+	return dst
+}
+
+// Int32ValueMap converts a string map of int32 pointers into a string
+// map of int32 values
+func Int32ValueMap(src map[string]*int32) map[string]int32 {
+	dst := make(map[string]int32)
+	for k, val := range src {
+		if val != nil {
+			dst[k] = *val
+		}
+	}
+	return dst
+}
+
 // Int64 returns a pointer to of the int64 value passed in.
 func Int64(v int64) *int64 {
 	return &v
