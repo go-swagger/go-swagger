8d0cd30919255799d25714d398c3b44edb7e46a4
Ivan Porto Carrero <ivan@flanders.co.nz>
2016-02-07 12:31:29 -0800

    update to build a release

--- cmd/swagger/commands/version_number.go
+++ cmd/swagger/commands/version_number.go
@@ -1,6 +1,6 @@
 package commands
 
 func init() {
-  Version = "v0.2.0-186-g4111d1f"
+  Version = "0.2.1"
 }
 
---
+++ hack/build-release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+version=`git describe`
+mkdir -p /usr/share/dist/{binaries,packages}
+gox -os="linux darwin windows" -output="/usr/share/dist/binaries/{{.Dir}}_{{.OS}}_{{.Arch}}" ./cmd/swagger
+
+cd /usr/share/dist
+mkdir -p /usr/share/dist/linux/amd64/usr/bin
+cp /usr/share/dist/binaries/swagger_linux_amd64 /usr/share/dist/linux/amd64/usr/bin/swagger
+fpm -t deb -s dir -C /usr/share/dist/linux/amd64 -v "$version" -n swagger --license "ASL 2.0" -a x86_64 -m "ivan@flanders.co.nz" --url "https://goswagger.io" usr
+fpm -t rpm -s dir -C /usr/share/dist/linux/amd64 -v "$version" -n swagger --license "ASL 2.0" -a x86_64 -m "ivan@flanders.co.nz" --url "https://goswagger.io" usr
