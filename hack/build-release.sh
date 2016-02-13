db18070aefea806a5547bbdf657258bf0bbdd392
Ivan Porto Carrero <ivan@flanders.co.nz>
2016-02-13 08:31:37 -0800

    change location for deb and rpm package

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -6,7 +6,7 @@ gox -os="linux darwin windows" -output="/drone/dist/binaries/{{.Dir}}_{{.OS}}_{{
 
 ls /drone/dist/binaries
 
-cd /drone/dist
+cd /drone/src/github.com/go-swagger/go-swagger
 mkdir -p /drone/dist/linux/amd64/usr/bin
 cp /drone/dist/binaries/swagger_linux_amd64 /drone/dist/linux/amd64/usr/bin/swagger
 fpm -t deb -s dir -C /drone/dist/linux/amd64 -v "$version" -n swagger --license "ASL 2.0" -a x86_64 -m "ivan@flanders.co.nz" --url "https://goswagger.io" usr
