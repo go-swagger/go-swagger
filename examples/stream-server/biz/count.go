3343a05bfb91af8b48c2d0de1e9d02fb90eb4726
Nelz <nelz9999@users.noreply.github.com>
2016-09-11 11:06:38 -0700

    Example project for a generating a streaming server (#662)
    * Example project for a generating a streaming server
    
    * Doc formatting
    
    * Better doc
    
    * Typo in doc
    
    * Package models is generated so should be ignored
    
    * Be sure to ignore generated models directory
    
    * For examples, prevailing wisdom says to check in generated code

---
+++ examples/stream-server/biz/count.go
@@ -0,0 +1,30 @@
+package biz
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/go-swagger/go-swagger/examples/stream-server/models"
+)
+
+// MyCounter is the concrete implementation
+type MyCounter struct{}
+
+// Down is the concrete implementation that spits out the JSON bodies
+func (mc *MyCounter) Down(max int64, w io.Writer) error {
+	if max == 11 {
+		return fmt.Errorf("we don't *do* elevensies")
+	}
+	e := json.NewEncoder(w)
+	for ix := int64(0); ix <= max; ix++ {
+		r := max - ix
+		fmt.Printf("Iteration %d\n", r)
+		e.Encode(models.Mark{Remains: &r})
+		if ix != max {
+			time.Sleep(1 * time.Second)
+		}
+	}
+	return nil
+}
