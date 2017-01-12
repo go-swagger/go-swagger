2df91b564ef97bfe170cbf1164f31f21095c68fb
JrCs <90z7oey02@sneakemail.com>
2017-01-12 14:10:53 +0100

    Add default imports in templates
    In case go-imports failed to scan vendor directory the generated code
    have missing imports.
    This commit add default imports that will be remove by go-imports if the
    generate code not needed them.
    
    Fix #590

--- generator/model.go
+++ generator/model.go
@@ -257,6 +257,7 @@ func makeGenDefinitionHierarchy(name, pkg, container string, schema spec.Schema,
 		defaultImports = []string{
 			"github.com/go-openapi/errors",
 			"github.com/go-openapi/runtime",
+			"github.com/go-openapi/swag",
 			"github.com/go-openapi/validate",
 		}
 	}
