18d1038c308e0f588563483693153b51c3247128
Andreas Muttscheller <andreas.muttscheller@codecentric.de>
2019-05-17 17:29:05 +0200

    Fix datarace between Serve() and Shutdown() on servers variable
    Fixes #1943
    
    Adding test for data race fix
    
    Run go generate ./generator
    
    Run gofmt -s
    
    Signed-off-by: Andreas Muttscheller <andreas.muttscheller@codecentric.de>

---
+++ generator/test_test.go
@@ -0,0 +1,63 @@
+package generator
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+func TestGenerateAndTest(t *testing.T) {
+	defer func() {
+		log.SetOutput(os.Stdout)
+	}()
+
+	cases := map[string]struct {
+		spec string
+	}{
+		"issue 1943": {
+			"../fixtures/bugs/1943/fixture-1943.yaml",
+		},
+	}
+
+	for name, cas := range cases {
+		var captureLog bytes.Buffer
+		log.SetOutput(&captureLog)
+
+		t.Run(name, func(t *testing.T) {
+			spec := filepath.FromSlash(cas.spec)
+
+			opts := testGenOpts()
+
+			opts.Target = filepath.Dir(spec)
+			_ = os.Mkdir(opts.Target, 0755)
+			opts.Spec = spec
+			opts.ExcludeSpec = false
+
+			fmt.Println(opts.Target)
+
+			err := GenerateServer("", nil, nil, &opts)
+			defer func() { _ = os.RemoveAll(opts.Target + "/models") }()
+			defer func() { _ = os.RemoveAll(opts.Target + "/restapi") }()
+
+			if err != nil {
+				t.Fatalf("Execute()=%s", err)
+			}
+
+			//fmt.Println(captureLog.String())
+			//assert.Contains(t, strings.ToLower(captureLog.String()), "creating generated file")
+			packages := filepath.Join(opts.Target, "...")
+
+			if p, err := exec.Command("go", "get", packages).CombinedOutput(); err != nil {
+				t.Fatalf("go get %s: %s\n%s", packages, err, p)
+			}
+
+			if p, err := exec.Command("go", "test", "-race", packages).CombinedOutput(); err != nil {
+				t.Fatalf("go test -race %s: %s\n%s", packages, err, p)
+			}
+		})
+	}
+}
