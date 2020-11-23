4bb9095b9cd642714acedb98a7146718c2b089e3
Frederic BIDON <fredbi@yahoo.com>
2020-11-24 00:22:36 +0100

    fixed generated package resolution for principal
    * fixes #2362
    
    Support pre v0.25 behavior regarding the specification of the principal (e.g. --principal {package}.{type})
    
    * v0.25 behavior is maintained (e.g. --principal github.com/example/auth.Principal), with package alias deconfliction
    * specifying the principal without a fully qualified package name (e.g. --principal auth.Principal)
      results in a package assumed to be within the generated target
    * as a special case of interest, this works when the principal is a type in generated models
      (e.g. --models-package models/generated --principal generated.Principal)
    * covers the edge case raised by #2362, that is, even in the case of models generated in some subdirectory
      (e.g. --models-package restapi/models --package restapi/models.User)
    
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- fixtures/bugs/2362/gen.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-rm -rf internal && mkdir internal
-swagger generate server \
-  --spec fixture-2362.yaml \
-  --target internal \
-  --model-package restapi/models \
-  --principal internal/restapi/models.Principal \
-  --exclude-main \
-  --name data-updater
