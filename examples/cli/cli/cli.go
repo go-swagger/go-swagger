84233b9c4a65e4575e6b5bd229e1493e140cc38c
Youyuan Wu <youyuanwu@outlook.com>
2021-04-04 21:46:28 -0700

    Support reading flag vars from config file
    Signed-off-by: Youyuan Wu <youyuanwu@outlook.com>

--- examples/cli/cli/cli.go
+++ examples/cli/cli/cli.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 
 	"github.com/go-swagger/go-swagger/examples/cli/client"
@@ -16,11 +17,19 @@ import (
 	"github.com/go-openapi/runtime"
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // debug flag indicating that cli should output debug logs
 var debug bool
+
+// config file location
+var configFile string
+
+// name of the executable
+var exeName string = filepath.Base(os.Args[0])
 
 // logDebugf writes debug log to stdout
 func logDebugf(format string, v ...interface{}) {
@@ -35,14 +44,8 @@ var maxDepth int = 5
 
 // makeClient constructs a client object
 func makeClient(cmd *cobra.Command, args []string) (*client.AToDoListApplication, error) {
-	hostname, err := cmd.Flags().GetString("hostname")
-	if err != nil {
-		return nil, err
-	}
-	scheme, err := cmd.Flags().GetString("scheme")
-	if err != nil {
-		return nil, err
-	}
+	hostname := viper.GetString("hostname")
+	scheme := viper.GetString("scheme")
 
 	r := httptransport.New(hostname, client.DefaultBasePath, []string{scheme})
 	r.Debug = debug
@@ -65,16 +68,23 @@ func makeClient(cmd *cobra.Command, args []string) (*client.AToDoListApplication
 
 // MakeRootCmd returns the root cmd
 func MakeRootCmd() (*cobra.Command, error) {
+	cobra.OnInitialize(initViperConfigs)
+
 	// Use executable name as the command name
 	rootCmd := &cobra.Command{
-		Use: filepath.Base(os.Args[0]),
+		Use: exeName,
 	}
+
 	// register basic flags
 	rootCmd.PersistentFlags().String("hostname", client.DefaultHost, "hostname of the service")
+	viper.BindPFlag("hostname", rootCmd.PersistentFlags().Lookup("hostname"))
 	rootCmd.PersistentFlags().String("scheme", client.DefaultSchemes[0], fmt.Sprintf("Choose from: %v", client.DefaultSchemes))
+	viper.BindPFlag("scheme", rootCmd.PersistentFlags().Lookup("scheme"))
 
 	// configure debug flag
 	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "output debug logs")
+	// configure config location
+	rootCmd.PersistentFlags().StringVar(&configFile, "config", "", "config file path")
 
 	// register security flags
 	if err := registerAuthInoWriterFlags(rootCmd); err != nil {
@@ -92,9 +102,36 @@ func MakeRootCmd() (*cobra.Command, error) {
 
 	return rootCmd, nil
 }
+
+// initViperConfigs initialize viper config using config file in '$HOME/.config/<cli name>/config.<json|yaml...>'
+// currently hostname, scheme and auth tokens can be specified in this config file.
+func initViperConfigs() {
+	if configFile != "" {
+		// use user specified config file location
+		viper.SetConfigFile(configFile)
+	} else {
+		// look for default config
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(path.Join(home, ".config", exeName))
+		viper.SetConfigName("config")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		logDebugf("Error: loading config file: %v", err)
+		return
+	}
+	logDebugf("Using config file: %v", viper.ConfigFileUsed())
+}
+
+// registerAuthInoWriterFlags registers all flags needed to perform authentication
 func registerAuthInoWriterFlags(cmd *cobra.Command) error {
 	/* */
 	cmd.PersistentFlags().String("x-todolist-token", "none", ``)
+	viper.BindPFlag("x-todolist-token", cmd.PersistentFlags().Lookup("x-todolist-token"))
 	return nil
 }
 
@@ -102,10 +139,7 @@ func registerAuthInoWriterFlags(cmd *cobra.Command) error {
 func makeAuthInfoWriter(cmd *cobra.Command) (runtime.ClientAuthInfoWriter, error) {
 	auths := []runtime.ClientAuthInfoWriter{}
 	/* */
-	XTodolistTokenKey, err := cmd.Flags().GetString("x-todolist-token")
-	if err != nil {
-		return nil, err
-	}
+	XTodolistTokenKey := viper.GetString("x-todolist-token")
 	auths = append(auths, httptransport.APIKeyAuth("x-todolist-token", "header", XTodolistTokenKey))
 	if len(auths) == 0 {
 		logDebugf("Warning: No auth params detected.")
--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/kr/pretty v0.2.1
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.4.1
 	github.com/pelletier/go-toml v1.8.1 // indirect
 	github.com/pkg/errors v0.9.1
