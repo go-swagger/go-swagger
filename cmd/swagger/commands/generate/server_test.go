321a584be7d92b51dabe784fc57e9518b0f3ba98
Matt Alexander <matt.alexander@pantheon.io>
2021-07-09 23:32:49 -0500

    Removed bracket that was causeing an error if StrictResponders and ImplementationPackage were both set for a server generation
    Signed-off-by: Matt Alexander <mattalexxpub@gmail.com>

--- cmd/swagger/commands/generate/server_test.go
+++ cmd/swagger/commands/generate/server_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/go-swagger/go-swagger/cmd/swagger/commands/generate"
-	flags "github.com/jessevdk/go-flags"
+	"github.com/jessevdk/go-flags"
 )
 
 func TestGenerateServer(t *testing.T) {
@@ -72,4 +72,45 @@ func TestGenerateServer_Checks(t *testing.T) {
 	m.Shared.CopyrightFile = "nowhere"
 	err := m.Execute([]string{})
 	assert.Error(t, err)
+}
+
+func TestRegressionIssue2601(t *testing.T) {
+	specs := []string{
+		"impl.yml",
+	}
+	log.SetOutput(ioutil.Discard)
+	defer log.SetOutput(os.Stdout)
+
+	base := filepath.FromSlash("../../../../")
+	for i, spec := range specs {
+		_ = t.Run(spec, func(t *testing.T) {
+			path := filepath.Join(base, "fixtures/codegen", spec)
+			generated, err := ioutil.TempDir(filepath.Dir(path), "generated")
+			if err != nil {
+				t.Fatalf("TempDir()=%s", generated)
+			}
+			defer func() {
+				_ = os.RemoveAll(generated)
+			}()
+			m := &generate.Server{}
+			_, _ = flags.Parse(m)
+			if i == 0 {
+				m.Shared.CopyrightFile = flags.Filename(filepath.Join(base, "LICENSE"))
+			}
+			m.Shared.Spec = flags.Filename(path)
+			m.Shared.Target = flags.Filename(generated)
+
+			// Error was coming from these two being set together
+			m.Shared.StrictResponders = true
+			m.ImplementationPackage = "github.com/go-swagger/go-swagger/fixtures/codegen/impl"
+
+			// Load new copy of template
+			m.Shared.AllowTemplateOverride = true
+			m.Shared.TemplateDir = flags.Filename(filepath.Join(base, "generator/templates"))
+
+			if err := m.Execute([]string{}); err != nil {
+				t.Error(err)
+			}
+		})
+	}
 }
