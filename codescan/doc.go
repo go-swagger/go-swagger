75f2a8068173b2f1ddb8550322229d07592161bc
3idey <elaidya225@gmail.com>
2026-01-20 17:34:48 +0200

    refactor: remove internal codescan package
    Remove the codescan package from go-swagger repository as it has been
    moved to its own standalone repository at github.com/3idey/codescan.
    
    Benefits of this change:
    - Allows codescan to have its own release cycle
    - Enables faster iterations and improvements without breaking go-swagger
    - Provides better separation of concerns
    - Users can use the code scanner as a standalone library
    
    The codescan functionality remains available via the external dependency.
    
    Closes #3274
    
    Signed-off-by: 3idey <elaidya225@gmail.com>

--- codescan/doc.go
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: Copyright 2015-2025 go-swagger maintainers
-// SPDX-License-Identifier: Apache-2.0
-
-/*
-Package codescan provides a scanner for go files that produces a swagger spec document.
-
-This package is intendnd for go1.11 onwards, and does support go modules.
-*/
-package codescan
